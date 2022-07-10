01ce83e11a2e12db48bb3ceec08c67f3e44e23bd
alberto@sgart.it <alberto@sgart.it>
2022-07-10 18:14:45 +0200

    minimal api added EF 6

--- Sgart.Net.MinimalAPI/Program.cs
+++ Sgart.Net.MinimalAPI/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using NLog;
 using NLog.Web;
+using Microsoft.EntityFrameworkCore;
 
 // imposto NLog per leggere da appsettings.json
 var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
@@ -9,6 +10,12 @@ logger.Debug("Sgart.it demo init");
 try
 {
     var builder = WebApplication.CreateBuilder(args);
+
+    // registro EF DB context usando InMemoryDatabase, add services to the container.
+    // Note: InMemoryDatabase da usare solo per Demo, non usare in produzione
+    builder.Services.AddDbContext<TodoDbContext>(opt => opt.UseInMemoryDatabase("SgartTodoList"));
+
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
     // Setup NLog for Dependency injection
     builder.Logging.ClearProviders();
@@ -37,6 +44,65 @@ try
         result = Math.Tan(num)
     });
 
+    // API Todo basate su TodoDbContext
+    app.MapGet("/todo", async (TodoDbContext db) => await db.Todos.ToListAsync());
+    app.MapGet("/todo/completed", async (TodoDbContext db) => await db.Todos.Where(x => x.Completed == true).ToListAsync());
+    // /todo/text/contains?text=ciao
+    app.MapGet("/todo/contains", async (string text, TodoDbContext db) =>
+        await db.Todos
+            .Where(x => x.Text != null && x.Text.Contains(text, StringComparison.InvariantCultureIgnoreCase))
+            .ToListAsync());
+    app.MapGet("/todo/{todoId}", async (int todoId, TodoDbContext db) =>
+        await db.Todos.FindAsync(todoId) is Todo todo
+            ? Results.Ok(todo)
+            : Results.NotFound());
+    app.MapPost("/todo", async (TodoInputDTO inputTodo, TodoDbContext db) =>
+    {
+    // validare sempre i parametri di ingresso
+        if (string.IsNullOrWhiteSpace(inputTodo.Text))
+            return Results.BadRequest("Invalid Text");
+
+        var todo = new Todo
+        {
+            Text = inputTodo.Text,
+            Completed = inputTodo.Completed
+        };
+
+        db.Todos.Add(todo);
+        await db.SaveChangesAsync();
+
+        return Results.Created($"/todo/{todo.TodoId}", todo);
+    });
+    app.MapPut("/todo/{id}", async (int id, TodoInputDTO inputTodo, TodoDbContext db) =>
+    {
+        var todo = await db.Todos.FindAsync(id);
+
+        if (todo is null)
+            return Results.NotFound();
+
+    // validare sempre i parametri di ingresso
+        if (string.IsNullOrWhiteSpace(inputTodo.Text))
+            return Results.BadRequest("Invalid Text");
+
+        todo.Text = inputTodo.Text;
+        todo.Completed = inputTodo.Completed;
+
+        await db.SaveChangesAsync();
+
+        return Results.NoContent();
+    });
+    app.MapDelete("/todo/{id}", async (int id, TodoDbContext db) =>
+    {
+        if (await db.Todos.FindAsync(id) is Todo todo)
+        {
+            db.Todos.Remove(todo);
+            await db.SaveChangesAsync();
+            return Results.Ok(todo);
+        }
+
+        return Results.NotFound();
+    });
+
     app.Run();
 }
 catch (Exception exception)
@@ -52,4 +118,22 @@ finally
 }
 
 // https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/record
-public record MultInputDTO(int V1, int V2);
+record MultInputDTO(int V1, int V2);
+
+record TodoInputDTO(string Text, bool Completed);
+
+class Todo
+{
+    public int TodoId { get; set; }
+    public string? Text { get; set; }
+    public bool Completed { get; set; }
+};
+
+// creo il context per il DB di EF
+class TodoDbContext : DbContext
+{
+    public TodoDbContext(DbContextOptions<TodoDbContext> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
--- Sgart.Net.MinimalAPI/README.md
+++ Sgart.Net.MinimalAPI/README.md
@@ -1,5 +1,13 @@
-# demo .NET 6 minimal web API + NLog
+# demo .NET 6 minimal web API + NLog + Entyty Framework 6
 
 ## Nuget
 
+### NLog
+
 dotnet add package NLog.Web.AspNetCore --version 5.0.0
+
+### EF 6
+
+dotnet add package Microsoft.EntityFrameworkCore.InMemory
+
+dotnet add package Microsoft.AspNetCore.Diagnostics.EntityFrameworkCore
